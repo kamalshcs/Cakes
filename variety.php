7531f0df7ca158cc8ff2393df0e4d661b096aec0
kamalshcs <51731277+kamalshcs@users.noreply.github.com>
2019-07-05 14:24:55 +0530

    Update variety.php

--- variety.php
+++ variety.php
@@ -109,7 +109,7 @@ Animations
       
 
 
-        <li><a href="aboutus.php" style="font-family:french script MT regular; color:palevioletred; font-size:250%">about us</a></li>
+        <li><a href="index.php" style="font-family:french script MT regular; color:palevioletred; font-size:250%">about us</a></li>
         <li><a href="contactus.php" style="font-family:french script MT regular; color:palevioletred; font-size:250%">speciality</a></li>
         <li><a href="variety.php" style="font-family:french script MT regular; color:palevioletred; font-size:250%">variety</a></li>
         <li><a href="recipe.php" style="font-family:french script MT regular; color:palevioletred; font-size:250%">recipe corner</a></li>
